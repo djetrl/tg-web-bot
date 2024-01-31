537a241e1d2021eb30522eea67b910f489d53427
djetrl <leshadjet@mail.ru>
2024-01-31 22:40:09 +0700

    31.01.2024

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Button from '../button/Button';
 import { useTelegram } from '../../hooks/useTelegram';
-import shop from './src/asset/icon/shop.svg';
-import { ReactSVG } from 'react-svg';
+import {ShoppingCartOutlined} from '@ant-design/icons'
 import './Header.css'
 const Header = () => {
   const {tg, onClose, user} = useTelegram();
@@ -12,7 +11,7 @@ const Header = () => {
       <span className='username'>
         {user?.username}
       </span>
-      <button>  <ReactSVG src={shop} className='shop'/></button>
+      <ShoppingCartOutlined className='shopIcon' />
     </div>
   );
 };
